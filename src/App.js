2a89feff0160728911226585855eef09a5fd0dcf
Kike <kike@Kikes-MacBook-Pro.local>
2020-07-24 22:26:50 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//IMPORTS
+//-Modules
+import React, {useRef, useEffect} from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+import gsap from 'gsap'
+//-Images
+import logoText from './imgs/logoText.png'
 
+//STYLED
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: #F2F5F2;
+  }
+`
+const LogoCircle = styled.div`
+  position: absolute;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -50%);
+  width: 140px;
+  height: 140px;
+  background: #F2F5F2;
+  border: 0 solid rgba(247,250,246,0.50);
+  box-shadow: -3px -3px 6px 0 #FAFCF9, 3px 3px 6px 0 #EBEDEA;
+  border-radius: 50%;
+`
+const LogoText = styled.img`
+  position: absolute;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -50%) scale(0.425);
+  
+`
+
+//MAIN COMPONENTS
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  let logoCirleRef = useRef()
+  var tl = gsap.timeline({repeat: -1})
+  useEffect(() => {
+    tl.to(logoCirleRef, {duration: 50, rotate: 360})
+  }, [tl])
+  
+
+  return (
+    <>
+      <GlobalStyle/>
+      <LogoCircle ref={el => logoCirleRef = el}/>
+      <LogoText src={logoText}/>
+    </>
+  )
+}
 export default App;
